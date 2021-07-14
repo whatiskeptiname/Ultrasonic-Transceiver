bc7fb78b2a4c4229562c19adb22d18aa98624132
susang <susangmree@gmail.com>
2021-07-14 14:47:54 +0545

    first commit

---
+++ TX_ultrasonic.ino
@@ -0,0 +1,43 @@
+char message[10];
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(3, OUTPUT);
+}
+
+void loop()
+{
+  send("Hello World!!!\n");
+}
+void send(String msg)
+{
+  byte ch;
+  unsigned int pos = 0;
+  unsigned int sz = msg.length();
+  while (pos < sz)
+  {
+    ch = msg.charAt(pos);
+    Serial.print((char)ch);
+    tone(3, 40000);
+    delay(10);
+    noTone(3);
+    for (int i = 0; i < 8; i++)
+    {
+      boolean b;
+      b = bitRead(ch, 7 - i);
+      if (b)
+      {
+        tone(3, 40000);
+        delay(2);
+      }
+      else
+      {
+        tone(3, 40000);
+        delay(4);
+      }
+      noTone(3);
+      delay(11);
+    }
+    pos++;
+  }
+}
